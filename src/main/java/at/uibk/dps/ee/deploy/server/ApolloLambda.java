fdcca3deb8b727d673decc07d671a8e02d7dc35e
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2021-07-06 11:26:24 +0200

    lambda handler for apollo (entry point of the apollo lambda function)

---
+++ src/main/java/at/uibk/dps/ee/deploy/server/ApolloLambda.java
@@ -0,0 +1,19 @@
+package at.uibk.dps.ee.deploy.server;
+
+import at.uibk.dps.ee.deploy.run.ImplementationRunBare;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
+
+public class ApolloLambda implements RequestHandler<Map<String, String>, String> {
+
+    @Override public String handleRequest(Map<String, String> event, Context context) {
+        ImplementationRunBare implementationRunBare = new ImplementationRunBare();
+
+        String configString = event.get("config");
+        String specString = event.get("spec");
+        String inputString = event.get("input");
+
+        return implementationRunBare.implement(inputString, specString, configString).toString();
+    }
+}
